206456bd265917bbdab2b0da5a49342c93c48af8
Marese Casia <73417129+mars2c@users.noreply.github.com>
2023-05-26 20:43:15 +0800

    Add files via upload

---
+++ 1ST Year/CMSC 11/Machine Problem 4/CasiaStudy3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    printf("Hello Philippines and hello world. Tira-tira!\n");
+    printf("Dahil hindi pwede tayong dalawa\n");
+    printf("Don't hate yourself in the morning -- sleep till noon.\n");
+    printf("Tonight you will pay the wages of sin; \nDon't forget to leave a tip.\n");
+    printf("You are not dead yet. \tBut watch for further reports...\n");
+    printf("You have the capacity to learn from mistakes.\nYou'll learn a lot today.\n");
+    printf("Basta ya!\n");
+    return 0;
+}
