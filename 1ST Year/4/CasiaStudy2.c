369fad17c7aea95d89a1766f05c4438d49674f2a
Marese Casia <73417129+mars2c@users.noreply.github.com>
2023-05-26 20:43:51 +0800

    Delete 1ST Year/4 directory

--- 1ST Year/4/CasiaStudy2.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Hello Philippines and hello world. Tira-tira!");
-    printf("Dahil hindi pwede tayong dalawa");
-    printf("Don't hate yourself in the morning -- sleep till noon.");
-    return 0;
-}
--- 1ST Year/4/tempCodeRunnerFile.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    printf("Hello Philippines and hello world. Tira-tira!\n");
-    return 0;
-}
